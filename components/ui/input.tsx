ebc45da84aa31d8f29e9c5da9adcb084aa4565c0
TrungNguyen1811 <trung4atlam@gmail.com>
2024-09-23 22:29:40 +0700

    error vecel test

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
-
+/* eslint-disable @typescript-eslint/no-empty-interface */
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
+/* eslint-enable @typescript-eslint/no-empty-interface */
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
